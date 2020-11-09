2881e7ca8d5aa92215f091577b2268d672ea5689
Hasebe Naoki <hasebenaoki@hasebenaokinoMacBook-Pro.local>
2020-11-10 07:51:34 +0900

    オブジェクトの生成と判定、Test時の動作完成

---
+++ Assets/TestFile/ExampleTestScrit.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ExampleTestScrit
+    {
+        //BreforeAfterTestAttributeを持たせる
+        [Test, BeforeAfterTest]
+        public void ExampleTestScritSimplePasses()
+        {
+            Debug.Log("実行");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ExampleTestScritWithEnumeratorPasses()
+        {
+            Debug.Log("1");
+            yield return null;
+            Debug.Log("2");
+            yield return null;
+            Debug.Log("3");
+            yield return null;
+        }
+    }
+
+    public class BeforeAfterTestAttribute : NUnitAttribute, ITestAction
+    {
+        //Testだと一回ごとに呼び出され、Suiteだと一回だけしか呼ばない
+        public ActionTargets Targets => ActionTargets.Test;
+
+        public void AfterTest(ITest test)
+        {
+            Debug.Log("Test後に呼ばれている");
+        }
+
+        public void BeforeTest(ITest test)
+        {
+            Debug.Log("Test前に呼ばれているよ");
+        }
+    }
+}
---
+++ Assets/TestFile/SpherePositionTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor;
+
+namespace Tests
+{
+    public class SpherePositionTest:IPrebuildSetup ,IPostBuildCleanup
+    {
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SpherePositionTestSimplePasses()
+        {
+            
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SpherePositionTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        public void Setup()
+        {
+            string path = "Assets/Prefabs/Cube.prefab";
+            var obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        }
+
+        public void Cleanup()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
